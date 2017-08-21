c0c59f00bbfff6fa880b1e0c456d1bf5dc03c58e
thirug010 <30732819+thirug010@users.noreply.github.com>
2017-08-20 22:39:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ restService.use(bodyParser.json());
 
 restService.post('/link', function(req, res) {
     var userInfo = JSON.stringify(req.body.originalRequest.data.user);
+    var parameters = JSON.stringify(req.body.result.parameters);
     var deviceName = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceName ? req.body.result.parameters.deviceName : "No such Device in your Home"
     var deviceAction = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceAction ? req.body.result.parameters.deviceAction : "No such Action supported for all devies in your Home"
     
     var speech = deviceName + " is " + deviceAction;
     
-    var url = "http://smarthome2707.ddns.net/wapi/smartLinkDevice?deviceName="+deviceName+"&deviceAction=" + deviceAction +"&userInfo=" + userInfo   
+    var url = "http://smarthome2707.ddns.net/wapi/smartLinkDevice?deviceName="+deviceName+"&deviceAction=" + deviceAction +"&userInfo=" + userInfo + '&parameters=' + parameters
     http.get(url, function(response) {
           var finalData = "";
 
