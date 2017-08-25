5e7f4c63f7a5dddce1464a387fc9743577eed574
thirug010 <30732819+thirug010@users.noreply.github.com>
2017-08-25 09:37:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ restService.post('/link', function(req, res) {
     //var bodyStr  = JSON.stringify(req.body);
     var actionInfo = JSON.stringify(req.body.result.action);
     var userInfo   = JSON.stringify(req.body.originalRequest.data.user);
-    var parameters = JSON.stringify(req.body.result.parameters);
+    var parameters = JSON.stringify(req.body.result.parameters.replace('device-sub','devicesub'));
     var deviceName = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceName ? req.body.result.parameters.deviceName : "No such Device in your Home"
     var deviceAction = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceAction ? req.body.result.parameters.deviceAction : "No such Action supported for all devies in your Home"
     
