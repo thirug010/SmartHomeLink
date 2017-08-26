b960e0eef568cd3558d86fd5f3acb1622a6217b1
thirug010 <30732819+thirug010@users.noreply.github.com>
2017-08-26 08:25:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ restService.get('/linkget', function (req, res)
   res.send('Get Response')
 });
 
-restService.post('/link', function(req, res) {  
-    var userInfo   = JSON.stringify(req.body.originalRequest && req.body.originalRequest.data && req.body.originalRequest.data.user ? req.body.originalRequest.data.user : {});
-    var parameters = JSON.stringify( req.body.result && req.body.result.parameters ? req.body.result.parameters : {} ).replace('device-sub','devicesub');
-    var actionInfo = JSON.stringify(req.body.result && req.body.result.action ? action : {})
+restService.post('/link', function(req, res) { 
+    var userInfo = "";
+    var parameters = "";
+    var actionInfo = "";
+    /*
+    userInfo   = JSON.stringify(req.body.originalRequest && req.body.originalRequest.data && req.body.originalRequest.data.user ? req.body.originalRequest.data.user : {});
+    parameters = JSON.stringify( req.body.result && req.body.result.parameters ? req.body.result.parameters : {} ).replace('device-sub','devicesub');
+    actionInfo = JSON.stringify(req.body.result && req.body.result.action ? action : {})
+    */
     var deviceName = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceName ? req.body.result.parameters.deviceName : "No such Device in your Home"
     var deviceAction = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceAction ? req.body.result.parameters.deviceAction : "No such Action supported for all devies in your Home"
     
