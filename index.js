26ec3333eca4571e19a611fa14625b65859df0ef
thirug010 <30732819+thirug010@users.noreply.github.com>
2017-08-26 13:01:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ restService.post('/link', function(req, res) {
     var deviceName = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceName ? req.body.result.parameters.deviceName : "No such Device in your Home"
     var deviceAction = req.body.result && req.body.result.parameters && req.body.result.parameters.deviceAction ? req.body.result.parameters.deviceAction : "No such Action supported for all devies in your Home"
     
-    var speech = deviceName + " is the action " + deviceAction;
+    var speech = "Action completed";
     
     var url = "http://smarthome2707.ddns.net/wapi/smartLinkDevice?userInfo=" + userInfo + '&parameters=' + parameters + '&actionInfo=' + actionInfo
     http.get(url, function(response) {
